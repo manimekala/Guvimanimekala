0d56f90a28df899c9c7509810fed065d5fe5ce80
manimekala <manimekala1996@gmail.com>
2016-09-14 10:18:09 +0530

    Create Oddnointerval.java

---
+++ Oddnointerval.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Oddnointerval {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+int m=sc.nextInt();
+int s=sc.nextInt();
+for(int i=m;i<=s;i++)
+{
+	if(i%2!=0)
+	{
+		System.out.println(i);
+	}
+}
+
+	}
+
+}
